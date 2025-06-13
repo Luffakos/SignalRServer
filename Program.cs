c93e56832597b34294b596293ad735d537dd49af
Luffakos <ant1992sot@yahoo.gr>
2025-06-13 16:01:32 +0300

    Initial SignalR server

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
+using Microsoft.AspNetCore.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "SignalR Server is running!");
-
-app.MapHub<GameHub>("/gamehub");
+app.MapHub<QuizHub>("/quizhub");
 
 app.Run();
+
+public class QuizHub : Microsoft.AspNetCore.SignalR.Hub
+{
+    public async Task SendAnswer(string user, string answer)
+    {
+        await Clients.All.SendAsync("ReceiveAnswer", user, answer);
+    }
+}
